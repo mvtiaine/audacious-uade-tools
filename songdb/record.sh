d58c0d08025174d10e955cce0614d7404fd9089a
Matti Tiainen <mvtiaine@cc.hut.fi>
2025-09-22 01:11:38 +0300

    Tweak sox params

--- songdb/record.sh
+++ songdb/record.sh
@@ -31,8 +31,8 @@ TIMEOUT=${1:-0}
 THRESHOLD=${2:-0.67}
 MAX_RESULTS=${3:-10}
 
-if ! command -v sox &> /dev/null; then
-    echo "sox could not be found in PATH, please install it first."
+if ! command -v rec &> /dev/null; then
+    echo "rec could not be found in PATH, please install sox first."
     exit 1
 fi
 
@@ -58,7 +58,8 @@ rm -f "$TEMP_FILE"
 if [ "$TIMEOUT" -gt 0 ]; then
     echo "Recording for $TIMEOUT seconds... Press Ctrl+C to stop early"
 
-    sox -d "$TEMP_FILE" &
+    # Setting rate to 11025Hz may fail, but it's not a big issue (fpcalc will resample)
+    rec -r 11025 -c 1 "$TEMP_FILE" &
     SOX_PID=$!
     
     (sleep "$TIMEOUT" && kill $SOX_PID 2>/dev/null) &
@@ -70,11 +71,12 @@ else
     echo "Recording... Press Ctrl+C to stop and process"
     trap 'echo "Processing fingerprint..."; fpcalc -length 9999 -plain "$TEMP_FILE" \
                                           | ./audio_match.sc - $THRESHOLD $MAX_RESULTS; \
-                                          rm "$TEMP_FILE"; exit' INT
+                                          rm -f "$TEMP_FILE"; exit' INT
  
-    sox -d "$TEMP_FILE" &
+    # Setting rate to 11025Hz may fail, but it's not a big issue (fpcalc will resample)
+    rec -r 11025 -c 1 "$TEMP_FILE" &
     SOX_PID=$!
     
     wait $SOX_PID
 fi
-rm "$TEMP_FILE"
+rm -f "$TEMP_FILE"
